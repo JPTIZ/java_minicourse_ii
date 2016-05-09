be9cefea8f7046063f7b6bda6c444e92352371d9
João Paulo Taylor Ienczak Zanette <profissional.jptiz@gmail.com>
2016-05-09 20:08:34 -0300

    placeholder directory

---
+++ class_4/MainWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+    public static final long serialVersionUID = 1L;
+
+    private JMenuItem
+
+    public MainWindow(Database database) {
+    }
+}
